acc843d628b199574c51e2e93744ec371ab803d8
Dang Xuan Anh <dangxuananh1997@gmail.com>
2018-10-31 00:09:58 +0700

    add XML response action config + HomeAction 🌃

---
+++ src/java/action/client/HomeAction.java
@@ -0,0 +1,16 @@
+package action.client;
+
+/**
+ *
+ * @author dangxuananh1997
+ */
+public class HomeAction {
+    
+    public HomeAction() {
+    }
+    
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
--- src/java/action/client/ViewProductAction.java
+++ src/java/action/client/ViewProductAction.java
@@ -1,5 +1,10 @@
 package action.client;
 
+import com.opensymphony.xwork2.Action;
+import dto.ProductType;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 /**
  *
  * @author dangxuananh1997
@@ -8,13 +13,18 @@ public class ViewProductAction {
 
     // Inputs
     private String searchValue;
+    private ProductType productType;
     private int page;
+    
+    // Outputs
+    InputStream result;
     
     public ViewProductAction() {
     }
     
     public String execute() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.result = new ByteArrayInputStream("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><foo><bar>Đặng</bar></foo>".getBytes("UTF-8"));
+        return Action.SUCCESS;
     }
 
     public String getSearchValue() {
@@ -31,6 +41,22 @@ public class ViewProductAction {
 
     public void setPage(int page) {
         this.page = page;
+    }
+
+    public InputStream getResult() {
+        return result;
+    }
+
+    public void setResult(InputStream result) {
+        this.result = result;
+    }
+
+    public ProductType getProductType() {
+        return productType;
+    }
+
+    public void setProductType(int productType) {
+        this.productType = ProductType.valueOf(productType);
     }
     
 }
